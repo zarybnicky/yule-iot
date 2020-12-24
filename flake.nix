88a227322d247d4284078694c0450f85feed9e54
Jakub Zárybnický <kubaz@inuits.eu>
2020-12-24 12:21:54 +0100

    Expand host config

--- flake.nix
+++ flake.nix
@@ -32,8 +32,8 @@
       modules = [
         { nixpkgs.overlays = [ self.overlay ]; }
         (nixpkgs + "/nixos/modules/profiles/qemu-guest.nix")
-        ({ pkgs, ... }: {
 
+        ({ pkgs, ... }: {
           system.configurationRevision = nixpkgs.lib.mkIf (self ? rev) self.rev;
           nix.package = pkgs.nixUnstable;
           nix.extraOptions = ''
@@ -56,30 +56,22 @@
           fileSystems."/" = { device = "/dev/sda1"; fsType = "ext4"; };
 
           networking.firewall.allowPing = true;
-          networking.firewall.allowedTCPPorts = [ 22 80 443 ];
+          networking.firewall.allowedTCPPorts = [ 22 80 443 5672 ];
           networking.hostName = "charlie";
           networking.domain = "z";
 
           system.autoUpgrade = {
             enable = true;
-            flake = github:zarybnicky/yule-iot;
+            flake = github:zarybnicky/yule-iot/master;
           };
 
           environment.systemPackages = [
             pkgs.git pkgs.cachix
           ];
 
+          security.sudo.wheelNeedsPassword = false;
           services.openssh.enable = true;
           services.openssh.permitRootLogin = "yes";
-          # services.mysql = {
-          #   enable = true;
-          #   package = pkgs.mariadb;
-          #   ensureDatabases = ["iot"];
-          #   ensureUsers = [{
-          #     name = "iot";
-          #     ensurePermissions = { ".*" = "ALL PRIVILEGES"; };
-          #   }];
-          # };
 
           users.mutableUsers = false;
           users.users.iot = {
@@ -93,7 +85,193 @@
           users.users.root.openssh.authorizedKeys.keys = [
             "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDSEpOb1URHEE1JmN6XETlURLOhmy53d8CS22WixcqWisaNKluU5skD9oceFLatyWw85vFWwI6sT7RI+dk6RlBEpEKdzlwoW5wMhdjK+v6gdH1LFjEp/shEoNsu7oKSunzeNQ1ZY8holUmQ8lghGy+jkXX8ANXJNl5kVvhFU+22p8ivVibyO5gjfa6ZFzQvrt6ifq38qDYF5eqds/HkuSnc9tg8B6ilXkDo3FNneyoK9iVJ6l0M/sx0pZoWylFE8348k9LZMDLRN82uUTBsxlZqHFIJqf0UWq1OYInlExOgb12i3WTdFAqFrXz9fS9TIbxag7+Zd90vlAuatb1Sd4cr kuba.zarybnicky@post.cz"
           ];
+        })
 
+        ({ pkgs, config, ... }: {
+          services.nginx = {
+            enable = true;
+            enableReload = true;
+            recommendedGzipSettings = true;
+            recommendedOptimisation = true;
+            recommendedProxySettings = true;
+            virtualHosts."iot.zarybnicky.com" = {
+              enableACME = true;
+              forceSSL = true;
+              root = getSrc ./web;
+              locations."/grafana" = {
+                proxyPass = "http://127.0.0.1:3000";
+                proxyWebsockets = true;
+              };
+              locations."/loki" = {
+                proxyPass = "http://127.0.0.1:3100";
+                proxyWebsockets = true;
+              };
+              locations."/prometheus" = {
+                proxyPass = "http://127.0.0.1:9090";
+                proxyWebsockets = true;
+              };
+              locations."/rabbitmq" = {
+                proxyPass = "http://127.0.0.1:15672";
+                proxyWebsockets = true;
+              };
+              # locations."/alertmanager" = {
+              #   proxyPass = "http://127.0.0.1:9093";
+              #   proxyWebsockets = true;
+              # };
+            };
+          };
+
+          services.postgresql = {
+            enable = true;
+            extraPlugins = [ pkgs.timescaledb ];
+            extraConfig = "shared_preload_libraries = 'timescaledb'";
+            ensureDatabases = ["iot"];
+            ensureUsers = [{
+              name = "iot";
+              ensurePermissions = { ".*" = "ALL PRIVILEGES"; };
+            }];
+          };
+
+          services.grafana = {
+            enable = true;
+            rootUrl = "https://iot.zarybnicky.com/grafana/";
+            auth.anonymous.enable = true;
+            auth.anonymous.org_role = "Editor";
+          };
+
+          services.prometheus = {
+            enable = true;
+            webExternalUrl = "https://iot.zarybnicky.com/prometheus/";
+            globalConfig.scrape_interval = "15s";
+            rules = [];
+            scrapeConfigs = [
+              {
+                job_name = "node";
+                static_configs = [{
+                  targets = [ "127.0.0.1:${toString config.services.prometheus.exporters.node.port}" ];
+                }];
+              } {
+                job_name = "postgres";
+                static_configs = [{
+                  targets = [ "127.0.0.1:${toString config.services.prometheus.exporters.postgres.port}" ];
+                }];
+              } {
+                job_name = "rabbitmq";
+                static_configs = [{
+                  targets = [ "127.0.0.1:15692" ];
+                }];
+              }
+            ];
+          };
+          services.prometheus.exporters.postgres = {
+            enable = true;
+            runAsLocalSuperUser = true;
+          };
+          services.prometheus.exporters.node = {
+            enable = true;
+            enabledCollectors = [
+              "logind"
+              "systemd"
+            ];
+            disabledCollectors = [
+              "textfile"
+            ];
+          };
+          # services.prometheus.alertManagers = [{
+          #   scheme = "http";
+          #   path_prefix = "/alertmanager";
+          #   static_configs = [{ targets = [ "localhost" ]; }];
+          # }];
+          # services.prometheus.alertmanager = {
+          #   enable = true;
+          #   webExternalUrl = "https://iot.zarybnicky.com/alertmanager/";
+          #   configuration = {
+          #   };
+          # };
+
+          services.rabbitmq = {
+            enable = true;
+            listenAddress = "";
+            plugins = [ "rabbitmq_management" "rabbitmq_prometheus" ];
+            configItems = {
+              # "auth_backends.1.authn" = "rabbit_auth_backend_ldap";
+              # "auth_backends.1.authz" = "rabbit_auth_backend_internal";
+            };
+          };
+
+          services.loki = {
+            enable = true;
+            configFile = pkgs.writeText "loki.yaml" ''
+              auth_enabled: false
+              server:
+                http_listen_port: 3100
+              ingester:
+                lifecycler:
+                  address: 0.0.0.0
+                  ring:
+                    kvstore:
+                      store: inmemory
+                    replication_factor: 1
+                  final_sleep: 0s
+                chunk_idle_period: 1h
+                max_chunk_age: 1h
+                chunk_target_size: 1048576k
+                chunk_retain_period: 30s
+                max_transfer_retries: 0
+              schema_config:
+                configs:
+                  - from: 2020-12-24
+                    store: boltdb-shipper
+                    object_store: filesystem
+                    schema: v11
+                    index:
+                      prefix: index_
+                      period: 24h
+              storage_config:
+                boltdb_shipper:
+                  active_index_directory: /var/lib/loki/boltdb-shipper-active
+                  cache_location: /var/lib/loki/boltdb-shipper-cache
+                  cache_ttl: 24h
+                  shared_store: filesystem
+                filesystem:
+                  directory: /var/lib/loki/chunks
+              limits_config:
+                reject_old_samples: true
+                reject_old_samples_max_age: 168h
+              chunk_store_config:
+                max_look_back_period: 0s
+              table_manager:
+                retention_deletes_enabled: false
+                retention_period: 0s
+            '';
+          };
+          systemd.services.promtail = let
+            promTail = pkgs.writeText "promtail.yaml" ''
+              server:
+                http_listen_port: 28183
+                grpc_listen_port: 0
+              positions:
+                filename: /tmp/positions.yaml
+              clients:
+                - url: http://127.0.0.1:3100/loki/api/v1/push
+              scrape_configs:
+                - job_name: journal
+                  journal:
+                    max_age: 12h
+                    labels:
+                      job: systemd-journal
+                      host: iot
+                  relabel_configs:
+                    - source_labels: ['__journal__systemd_unit']
+                      target_label: 'unit'
+            '';
+          in {
+            description = "Promtail service for Loki";
+            wantedBy = [ "multi-user.target" ];
+            serviceConfig = {
+              ExecStart = "${pkgs.grafana-loki}/bin/promtail --config.file ${./promtail.yaml}";
+            };
+          };
         })
       ];
     };
